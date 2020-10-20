ac6daf3f1da6a797844093a855d8e41512c47f62
Mario Bischoff <mariobischoffneto@hotmail.com>
2020-10-20 20:48:16 -0300

    adicionei o codigo do device mqtt

--- backends/mqtt-backend/broker.js
+++ backends/mqtt-backend/broker.js
@@ -1,18 +1,7 @@
-const port = 1883;
-const wsPort = 9001;
-
 const aedes = require("aedes")();
-const ws = require("websocket-stream");
-
 const server = require("net").createServer(aedes.handle);
-const httpServer = require("http").createServer();
-ws.createServer({ server: httpServer }, aedes.handle);
+const port = 1883;
 
 server.listen(port, function () {
   console.log("server started and listening on port " + port);
-});
-
-httpServer.listen(wsPort, function () {
-  console.log("Aedes MQTT-WS listening on port " + wsPort);
-  aedes.publish({ topic: "aedes/hello", payload: "I'm broker " + aedes.id });
 });
---
+++ devices/device_mqtt/device_mqtt.ino
@@ -0,0 +1,118 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Bischoff";
+const char* password = "Kryptos12";
+const char* mqttServer = "192.168.0.109";
+const int mqttPort = 1883;
+
+const int pinPump = 4;
+//const int pinPump = LED_BUILTIN;
+const int pinHumidity = 0;
+
+// Timers auxiliar variables
+long now = millis();
+long lastMeasure = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("WiFi connected - ESP IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(String topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+
+  if(topic=="esp8266/pump"){
+      Serial.print("Changing Pump to ");
+      if(messageTemp == "on"){
+        digitalWrite(pinPump, LOW);
+        Serial.print("On");
+      }
+      else if(messageTemp == "off"){
+        digitalWrite(pinPump, HIGH);
+        Serial.print("Off");
+      }
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.subscribe("esp8266/pump");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+float readHumidity(void) {
+  int value = analogRead(pinHumidity);
+  float valuePercent;
+  valuePercent = 100 * ((978-(float)value) / 978);
+  return valuePercent;
+}
+
+void setup() {
+  pinMode(pinPump, OUTPUT);
+  
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  if (!client.loop())
+    client.connect("esp8266");
+
+  now = millis();
+
+  if (now - lastMeasure > 30000) {
+    lastMeasure = now;
+
+    float humidityPercent = readHumidity();
+    char humidity[10];
+    sprintf(humidity, "%f", humidityPercent);
+
+    client.publish("esp8266/humidity", humidity);
+    
+    Serial.print("Humidity: ");
+    Serial.println(humidity);
+  }
+}
